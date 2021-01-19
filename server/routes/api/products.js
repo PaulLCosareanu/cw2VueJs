422143e219a247c1a9ef11c6f6819aecac5d896b
PaulLCosareanu <paulluciancosareanu@gmail.com>
2021-01-19 22:37:33 +0000

    Initial backend created

--- server/routes/api/products.js
+++ server/routes/api/products.js
@@ -1,14 +1,52 @@
 const express = require("express")
-const mongoDb  = require("mongodb")
+const mongodb  = require("mongodb")
 
 
 const router= express.Router();
 
-//get
+//get products////////////////////////////////////////////////////
 
-router.get("/",(req,res)=>{
-    res.send("hello")
+router.get("/",async(req,res)=>{
+    const products=await loadProductsCollection();
+    res.send(await products.find({}).toArray());
 })
 
+//add products//////////////////////////////////////////////////////////////
+
+router.post("/", async(req,res)=>{
+    const products=await loadProductsCollection();
+    await products.insertOne({
+        subject: req.body.subject,
+        location:req.body.location,
+        price:req.body.price,
+        quantity:req.body.quantity,
+        img_link:req.body.img
+    });
+    res.status(201).send();
+})
+
+//delete products///////////////////////////////////////////////
+
+router.delete("/:id", async(req,res)=>{
+    const products=await loadProductsCollection();
+    await products.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
+    res.status(200).send();
+})
+
+
+//update products*************************************************
+
+
+
+async function loadProductsCollection(){
+    const client=await mongodb.MongoClient.connect("mongodb+srv://UserMongo:1234%40Middlesex@products.ysvpf.mongodb.net/vue?retryWrites=true&w=majority", { useNewUrlParser: true })
+
+    return client.db("vue").collection("products")
+
+
+
+
+
+}
 
 module.exports = router;
